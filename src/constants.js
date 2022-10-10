d05943799370c84f806039bc33d297df08e58e73
Shangzheng Ji <shangj3@vt.edu>
2022-10-10 01:05:03 -0400

    merged

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const SAT_API_KEY = "9FQ7WK-4XHMG9-AJ5PH9-4XMN";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "localhost:3000"; // your backend endpoint when deploy
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
